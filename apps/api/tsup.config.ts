4b1029471bcb5c9235547b088bb5f25b6207291d
Lordsans-404 <entahapa08@gmail.com>
2026-09-13 14:43:29 +0700

    fix: fix reference ts

--- apps/api/tsup.config.ts
+++ apps/api/tsup.config.ts
@@ -4,5 +4,7 @@ export default defineConfig({
   entry: ['src/index.ts'],
   format: ['esm'],
   clean: true,
+  bundle: true,
+  splitting: false,
   noExternal: ['@morphic/db', '@morphic/shared'],
 });
