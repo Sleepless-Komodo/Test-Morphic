7f984a850271b6908ca4fa4db9814ff546f0f65c
Lordsans-404 <entahapa08@gmail.com>
2026-09-14 08:51:09 +0700

    fix: configure Vercel builds to use pre-compiled function

--- apps/api/tsup.config.ts
+++ apps/api/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/index.ts'],
+  entry: ['src/index.ts', 'src/index.vercel.ts'],
   format: ['esm'],
   clean: true,
   bundle: true,
